ff46374eae8d13841a7aeb31fac3f437f00fa538
Judah Perez <tythin4@gmail.com>
2017-03-29 10:49:13 -0700

    Base project reading in the file

---
+++ SpamFilterAutomata/SpamFilterAutomata/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpamFilterAutomata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var textStream = File.OpenText("messagefile.txt"))
+            {
+                while (!textStream.EndOfStream)
+                {
+                    Console.Write((char)textStream.Read());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
